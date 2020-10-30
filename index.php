bbb19367db676b3d4c11072aa250e7efbf7f2531
Travis Lehman <travlehman@gmail.com>
2020-10-29 20:45:50 -0400

    Backup site

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link rel="stylesheet" href="build/css/main.min.css">
+    <meta name="description" content="The personal website of Travis Lehman, a UI designer and Front End Developer from Lancaster Pennsylvania" />
+	<meta name="keywords" content="Designer, Web Designer, Lancaster, PA, Front End Developer, Freelance" />
+    <link rel="shortcut icon" href="resources/images/favicon.ico">
+    <link rel="icon" type="image/png" href="resources/images/favicon-16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="resources/images/favicon-32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="resources/images/favicon-96.png" sizes="96x96">
+	<link rel="apple-touch-icon" sizes="152x152" href="resources/images/favicon-152.png">
+	<title>Travis Lehman - Designer & Developer</title>
+</head>
+<body class="no-js">
+    <header class="mast">
+        <h1 class="mast__title">Travis Lehman</h1>
+        <p class="mast__tagline">UI Designer / Front End Designer</p>
+        <nav class="social">
+            <a class="social__link social__link--twitter" href="http://twitter.com/tehtrav" target="blank">
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg"><path d="M15.2.1a14.78 14.78 0 1 0 .01 29.55 14.78 14.78 0 0 0 0-29.56zm7.45 11.79l.01.48c0 4.92-3.74 10.6-10.6 10.6-2.1 0-4.05-.62-5.7-1.68a7.61 7.61 0 0 0 5.51-1.54c-1.63-.03-3-1.1-3.47-2.59a3.64 3.64 0 0 0 1.68-.06 3.73 3.73 0 0 1-3-3.65l.01-.05c.5.28 1.08.45 1.69.47A3.72 3.72 0 0 1 7.62 8.9a10.57 10.57 0 0 0 7.68 3.89 3.73 3.73 0 0 1 6.34-3.4 7.44 7.44 0 0 0 2.37-.9c-.28.87-.87 1.6-1.64 2.06a7.44 7.44 0 0 0 2.14-.59c-.5.75-1.13 1.4-1.86 1.93z" fill-rule="nonzero"/></svg>
+                Twitter
+            </a>
+            <a class="social__link social__link--dribbble" href="http://dribbble.com/trav" target="blank">
+                <svg width="29" height="29" xmlns="http://www.w3.org/2000/svg"><path d="M28.67 11.69A14.33 14.33 0 0 0 .29 14.57a14.43 14.43 0 0 0 2.45 8.02 14.44 14.44 0 0 0 9 6.03 14.32 14.32 0 0 0 16.1-8.46 14.25 14.25 0 0 0 .83-8.47zm-1.8 2.76c-.18-.04-4.33-.93-8.53-.4a46.17 46.17 0 0 0-1.08-2.4c4.83-1.98 6.8-4.82 6.82-4.85a12.18 12.18 0 0 1 2.78 7.65zM22.7 5.4c-.03.04-1.77 2.7-6.4 4.43a62.2 62.2 0 0 0-4.57-7.14 12.22 12.22 0 0 1 10.97 2.7zm-11.02-2.7h-.04.04zm-2.3.81c.17.22 2.44 3.29 4.56 7.06a45.29 45.29 0 0 1-11.3 1.5C3.43 8.25 5.97 5.1 9.4 3.5zM2.39 14.57l.01-.38c.22 0 6.27.14 12.57-1.75.35.69.69 1.39 1 2.08l-.49.15c-6.6 2.14-9.96 8.08-9.96 8.08a12.2 12.2 0 0 1-3.13-8.18zm12.24 12.25a12.2 12.2 0 0 1-7.52-2.6s2.35-5.11 9.6-7.64l.09-.03a50.95 50.95 0 0 1 2.61 9.3c-1.47.62-3.08.97-4.78.97zm6.85-2.09c-.13-.74-.78-4.33-2.39-8.75 3.95-.63 7.38.46 7.63.54a12.25 12.25 0 0 1-5.24 8.21z" fill-rule="nonzero"/></svg>
+                Dribbble
+            </a>
+            <a class="social__link social__link--github" href="http://github.com/tehtrav" target="blank">
+                <svg width="30" height="29" xmlns="http://www.w3.org/2000/svg"><path d="M15 0C6.72 0 0 6.67 0 14.9a14.9 14.9 0 0 0 10.26 14.12c.75.14 1.02-.32 1.02-.72l-.02-2.77c-4.17.9-5.05-1.75-5.05-1.75-.68-1.72-1.67-2.18-1.67-2.18-1.36-.92.1-.9.1-.9 1.51.1 2.3 1.53 2.3 1.53 1.34 2.27 3.52 1.62 4.37 1.23.14-.96.52-1.61.95-1.99-3.33-.37-6.83-1.65-6.83-7.36 0-1.62.59-2.95 1.54-4-.15-.37-.67-1.88.15-3.93 0 0 1.26-.4 4.13 1.52a14.38 14.38 0 0 1 7.5 0c2.87-1.92 4.13-1.52 4.13-1.52a5.3 5.3 0 0 1 .15 3.94 5.73 5.73 0 0 1 1.54 4c0 5.72-3.5 6.97-6.85 7.34.54.47 1.02 1.37 1.02 2.76l-.02 4.09c0 .4.27.86 1.03.71A14.9 14.9 0 0 0 30 14.89C30 6.67 23.28 0 15 0z" fill-rule="nonzero"/></svg>
+                Github
+            </a>
+            <a class="social__link social__link--linkedin" href="https://linkedin.com/in/travislehman/" target="blank">
+                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg"><path d="M23.42 22.07V16.2c0-3.14-1.68-4.6-3.91-4.6-1.8 0-2.6 1-3.06 1.7v-1.46h-3.4c.05.96 0 10.22 0 10.22h3.4v-5.7c0-.31.02-.62.11-.83.25-.61.8-1.24 1.74-1.24 1.23 0 1.72.93 1.72 2.3v5.47h3.4zM9.48 10.47c1.18 0 1.92-.8 1.92-1.77-.02-1-.74-1.77-1.9-1.77S7.58 7.7 7.58 8.7c0 .98.74 1.76 1.88 1.76h.02zM15 30a15 15 0 1 1 0-30 15 15 0 0 1 0 30zm-3.82-7.93V11.86h-3.4v10.2h3.4z" fill-rule="evenodd"/></svg>
+                LinkedIn
+            </a>
+        </nav>
+    </header>
+    
+
+    <article class="tile">
+        <h2 class="tile__title"><a href="http://webstaurantstore.com" target="blank">WebstaurantStore.com</a></h2>
+        <p class="tile__description">Since 2014 I’ve worked on a team of talented designers and developers, helping to make WebstaurantStore.com the most successful online retailer for restaurant products.</p>
+        <div class="tile__visual">
+            <a href="http://webstaurantstore.com" target="blank"><img src="resources/images/webstaurantstore.png" alt=""></a>
+        </div>
+    </article>
+    <article class="tile">
+        <h2 class="tile__title"><a href="http://pedalplayground.com" target="blank">PedalPlayground.com</a></h2>
+        <p class="tile__description">Pedal Playground is an interactive tool for helping musicians plan and lay out their guitar effects pedals. It features a JSON library of over 1000 pedals. It gets several hundred hits a day and can be seen shared on many music gear forums and discussion groups. </p>
+        <div class="tile__visual">
+            <a href="http://pedalplayground.com target="blank""><img src="resources/images/pedalplayground.png" alt=""></a>
+        </div>
+    </article>
+    <article class="tile">
+        <h2 class="tile__title"><a href="http://therestaurantstore.com" target="blank">TheRestaurantStore.com</a></h2>
+        <p class="tile__description">In 2015, I was responsible for the front end redesign of TheRestaurantStore.com. I facilitated the redesign of hundreds of pages, including a complex checkout process with shipping & pickup options, membership program, and an intuitive navigation structure to support over 1000 unique products.</p>
+        <div class="tile__visual">
+            <a href="http://therestaurantstore.com" target="blank"><img src="resources/images/therestaurantstore.png" alt=""></a>
+        </div>
+    </article>
+    <article class="tile">
+        <h2 class="tile__title"><a href="http://destinyscopes.com" target="blank">DestinyScopes.com</a></h2>
+        <p class="tile__description">Destiny Scopes is a hobby website built off of data from the game Destiny, by Bungie. It displays hidden stats on many of the optics available in the game, and allows players to make educated decisions on what weapons to use to suit their playstyle.</p>
+        <div class="tile__visual tile__visual--dark">
+            <a href="http://destinyscopes.com" target="blank"><img src="resources/images/destinyscopes.png" alt=""></a>
+        </div>
+    </article>
+</body>
+</html>
---
+++ resources/js/scripts.js
@@ -0,0 +1,311 @@
+$(document).ready(function(){
+
+    // Run function to ajax scope data
+	GetScopeData();
+
+	// Make modal on click
+	$("body").on( "click", ".scope", function() {
+
+		// store scroll in var
+		var scrollPosition = $('body').scrollTop();
+
+		// Add hash to URL
+		window.location.hash = '#' + $(this).attr('id');
+
+		// Set scroll to stored
+		$('html,body').scrollTop(scrollPosition);
+
+		// $(this).attr('hash');
+
+        // Build modal HTML from elements in the exisiting markup
+		$("body").addClass("inspect-open").append("<div class='inspect'><div class='inspect--overlay'></div></div>");
+		var $modal = $("<div class='inspect--window'></div>");
+		var $container = $("<div></div>").addClass("inspect--container");
+		var $image = $("<div></div>").addClass("inspect--image");
+		var $info = $("<div></div>").addClass("inspect--info");
+
+		$(this).find(".visual").clone().appendTo($image);
+		$(this).find(".name").clone().appendTo($info);
+		$(this).find(".description, .notes").clone().appendTo($info);
+		$(this).find(".stats").clone().appendTo($info);
+
+        $modal.append('<a class="inspect--close" href="#"></a>');
+        $image.appendTo($container);
+		$info.appendTo($container);
+		$container.appendTo($modal);
+		$modal.appendTo(".inspect");
+	});
+
+    // Close overlay when clicking close or outside modal
+	$("body").on("click",".inspect--close, .inspect--overlay", function(e) {
+
+		// store scroll in var
+		var scrollPosition = $('body').scrollTop();
+
+        // Remove URL hash
+        window.location.hash = "";
+
+		// Set scroll to stored
+		$('html,body').scrollTop(scrollPosition);
+
+        // Add CSS animation class
+        $(".inspect").addClass("animate-out");
+        $(".inspect-open").removeClass("inspect-open");
+        // Remove markup once animation is complete
+        setTimeout(function(){
+            $(".inspect").remove();
+        }, 310);
+	});
+
+	// Add .js class to body for js/no-js styling
+	$(".no-js").removeClass("no-js").addClass("js");
+
+	// Turn on HideSeek on search field
+	$('.search input').hideseek({
+		ignore: '.description',
+		attribute: 'title'
+	});
+
+	// Focus on load
+	$('.search input').focus();
+
+
+	// When checkbox is clicked, hide all scopes
+	// then show the checked ones by class
+	$("#legend :checkbox").click(function() {
+		$(".scope").hide();
+		$("#legend :checkbox:checked").each(function() {
+			$( "." + $(this).val() ).show();
+			console.log("." + $(this).val());
+		});
+	});
+
+    // Hide/Show of stats in list view
+    $(function(){
+        $("#show-stats-preference").change(function() {
+            $(".scope-list").toggleClass("show-stats", this.checked)
+        }).change();
+    });
+
+
+    $("input[type='checkbox']").each(function() {
+        var mycookie = $.cookie($(this).attr('name'));
+        if (mycookie && mycookie == "true") {
+            $(this).prop('checked', mycookie);
+        }
+    });
+    $("input[type='checkbox']").change(function() {
+        $.cookie($(this).attr("name"), $(this).prop('checked'), {
+            path: '/',
+            expires: 365
+        });
+    });
+
+	// Give #legend a width based on column size so it
+	// doesn't collapse when given fixed positioning
+	$("#legend").width( $(".secondary-column").width() );
+	$( window ).resize(function() {
+		$("#legend").width( $(".secondary-column").width() );
+	});
+
+
+
+});
+
+window.Scope = function( name, type, manufacturer, description, icon, zoom, notes, images, stats){
+	this.Name = name || "";
+	this.Type = type || "";
+	this.Manufacturer = manufacturer || "";
+	this.Description = description || "";
+	this.Icon = icon || "";
+	this.Zoom = zoom || "";
+	this.Notes = notes || "";
+	this.Images = [];
+	this.Stats = [];
+	for(var i in images)
+	{
+		if(this.Images.length === 0){
+			this.Images.push( new ScopeImage(images[i]) );
+		} else {
+			this.Images.push( new ScopeIcon(images[i]) );
+		}
+	}
+	for(var i in stats)
+	{
+		this.Stats.push(
+			new ScopeStat(
+				stats[i][0],
+				stats[i][1],
+				(stats.length > 1 ? stats[i][2] : null),
+				(stats.length > 2 ? stats[i][3] : null)
+			)
+		);
+	}
+}
+
+window.ScopeImage = function(url){
+	this.href = url;
+};
+
+window.ScopeIcon = function(url){
+	this.href = url;
+};
+
+window.ScopeStat = function(name, value, isGraph, suffix){
+	this.Name = name || "";
+	this.Value = parseFloat(value || 0);
+	this.Suffix = suffix || "";
+	this.IsGraph = isGraph || true;
+};
+
+
+window.GetScopeData = function(){
+	$.ajax({
+		url: "resources/data/scopes.json",
+		dataType: 'text',
+		type: "GET",
+		success: function(data){
+			data = $.parseJSON(data.replace(/\r\n/g, "").replace(/\t/g, ""));
+			var scopes = [];
+			for(var scope in data){
+				scopes.push( new Scope(
+					data[scope].Name || "",
+					data[scope].Type || "",
+					data[scope].Manufacturer || "",
+					data[scope].Description || "",
+					data[scope].Icon || "",
+					data[scope].Zoom || "",
+					data[scope].Notes || "",
+					data[scope].Image || [],
+					data[scope].Stats || []
+				));
+			}
+			RenderScopes(scopes);
+            hashScroll();
+            adaptiveBackground();
+
+			//RenderLegend(scopes);
+		}
+	});
+
+};
+
+window.RenderScopes = function(scopes){
+	var $column = $("#scope-list");
+
+	var $legend = $("#legend");
+	//$legend.append('<h4>Scopes</h4><ul class="nav"></ul>');
+
+	for(var i in scopes) {
+		var $scope = $("<article></article>")
+			.addClass("item").attr('id', scopes[i].Name.toLowerCase().replace(/\s+/g, "-").replace(/'/g, ''));
+		$scope.attr('title', scopes[i].Name)
+		$scope.addClass("scope");
+		$scope.addClass("st-" + scopes[i].Type.toLowerCase().replace(/\s+/g, "-"));
+		$scope.addClass("sm-" + scopes[i].Manufacturer.toLowerCase().replace(/\s+/g, "-"));
+		var $visual = $("<div></div>").addClass("visual");
+		var $details = $("<div></div>").addClass("details");
+		var $stats = $("<ul></ul>").addClass("stats");
+
+		for(var si in scopes[i].Stats) {
+			var $stat = $("<li>").addClass("stats-" + scopes[i].Stats[si].Name.toLowerCase().replace(/\s+/g, "-").replace(/'/g, ''));
+			$stat.append($("<span></span>").addClass("label").text(scopes[i].Stats[si].Name));
+			if(scopes[i].Stats[si].IsGraph) {
+				var $amount = $("<span></span>").addClass("amount");
+				var $graph = $("<div></div>").addClass("graph");
+				var $plot = $("<div></div>").addClass("plot");
+
+				$plot.addClass(scopes[i].Stats[si].Value >= 0 ? "positive" : "negative")
+				$plot.attr("data-size", scopes[i].Stats[si].Value)
+				$plot.css("width", Math.abs(scopes[i].Stats[si].Value) * 3)
+				$plot.appendTo($graph);
+				$graph.appendTo($stat);
+
+				$amount.text((scopes[i].Stats[si].Value >= 0 ? "+" : "") + scopes[i].Stats[si].Value);
+				$amount.appendTo($stat);
+			}
+			$stat.appendTo($stats);
+		}
+
+		$visual.append($('<img class="scope-ads" src="resources/images/scope-images/compressed/' + scopes[i].Name.toLowerCase().replace(/\s+/g, "-").replace(/'/g, '') + '-ads.png" />'));
+		$visual.append($('<img class="scope-hip" src="resources/images/scope-images/compressed/' + scopes[i].Name.toLowerCase().replace(/\s+/g, "-").replace(/'/g, '') + '.png" />'));
+
+		if ( scopes[i].Zoom != 0 || scopes[i].Zoom != "" ) {
+			$visual.append($('<p></p>').addClass('zoom').text(scopes[i].Zoom));
+		}
+
+        if ( scopes[i].Stats != "" ) {
+            $scope.addClass("has-stats");
+		}
+
+		$details.append($('<div></div>').addClass('icon').css('background-image', 'url(' + scopes[i].Icon + ')'));
+
+		$visual.appendTo($scope);
+		$details.append($('<h2></h2>').addClass('name').text(scopes[i].Name));
+        if ( scopes[i].Notes != "" ) {
+            $details.append($('<p></p>').addClass('notes').text(scopes[i].Notes));
+		}
+		$details.append($('<p></p>').addClass('description').text(scopes[i].Description));
+		$details.appendTo($scope);
+		$stats.appendTo($scope);
+		$scope.appendTo($column);
+
+		// Make Legend
+		var $link = $("<li class='nav-item'></li>");
+		var $anchor = $("<a class='nav-link'></a>").attr("href", "#" + scopes[i].Name.toLowerCase().replace(/\s+/g, "-").replace(/'/g, '')).html(scopes[i].Name);
+		$link.addClass("scope");
+		$link.addClass("st-" + scopes[i].Type.toLowerCase().replace(/\s+/g, "-"));
+		$link.addClass("sm-" + scopes[i].Manufacturer.toLowerCase().replace(/\s+/g, "-"));
+		$anchor.appendTo($link);
+		$link.appendTo("#legend ul");
+
+	}
+}
+
+
+
+window.adaptiveBackground = function(scopeType){
+	var abSettings      = {
+		selector:             '.scope-ads',
+		parent:               '.scope .zoom',
+		//exclude:              [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
+		normalizeTextColor:   false,
+		normalizedTextColors:  {
+			light:      "#fff",
+			dark:       "#000"
+			},
+			lumaClasses:  {
+			light:      "ab-light",
+			dark:       "ab-dark"
+		}
+	};
+
+	//$.adaptiveBackground.run(abSettings);
+
+	$('img.scope-ads').on('ab-color-found', function(ev,payload) {
+		$(this).parents('.scope').find('.zoom').css('background-color',payload.color);
+		console.log(payload.color);   // The dominant color in the image.
+	// console.log(payload.palette); // The color palette found in the image.
+	// console.log(ev);   // The jQuery.Event object
+	});
+
+}
+
+window.FilterByScopeType = function(scopeType){
+	if(scopeType.length == 0){
+		// show all
+		$("[data-scopetype").show();
+	} else {
+		// filter
+		$("[data-scopetype]").hide();
+		$("[data-scopetype='" + scopeType + "']").show();
+	}
+}
+
+window.hashScroll = function(){
+    // If hash is in the url, go to that place
+    if(window.location.hash) {
+        var hash = window.location.hash
+        $(hash).click();
+    }
+}
